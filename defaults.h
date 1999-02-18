bc9a0981896780f911b3e0d84a1b9c4a6140bc2f
hartmann <hartmann@ae8a25bf-5f0b-68bf-7ac2-24876b81758e>
1999-02-18 05:25:04 +0000

    Added sweep.h to CLEANUP in Makefile.in .
    Changed defaults.h to not use LINEDRAW if not using ncurses.
    
    Did something to configure.

--- defaults.h
+++ defaults.h
@@ -1,5 +1,5 @@
 /*********************************************************************
-* $Id: defaults.h,v 1.2 1999-02-10 23:49:25 hartmann Exp $
+* $Id: defaults.h,v 1.3 1999-02-18 05:25:04 hartmann Exp $
 *********************************************************************/
 
 #ifndef __DEFAULTS_H__
@@ -11,10 +11,17 @@
 #define DEFAULT_WIDTH 15
 #define DEFAULT_PERCENT 20
 #define DEFAULT_COLOR 1
-#define DEFAULT_LINEDRAW 1
+
 #define DEFAULT_NUMMINES 0
 #define DEFAULT_FASTSTART 0
 #define DEFAULT_ALERT 0
 #define VI_KEYS
+
+/* Don't use Linedraw by default if not using ncurses. */
+#ifdef HAVE_LIBNCURSES
+#define DEFAULT_LINEDRAW 1
+#else /* HAVE_LIBNCURSES */
+#define DEFAULT_LINEDRAW 0
+#endif /* HAVE_LIBNCURSES */
 
 #endif /* __DEFAULTS_H__ */
