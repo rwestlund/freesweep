7a8047f966224c5480b20de0bc9a3dc0dfbaed3c
hartmann <hartmann@ae8a25bf-5f0b-68bf-7ac2-24876b81758e>
2000-10-31 02:48:23 +0000

    This file is nedded for autoheader.

---
+++ acconfig.h
@@ -0,0 +1,30 @@
+/* acconfig.h
+   This file is in the public domain.
+
+   Descriptive text for the C preprocessor macros that
+   the distributed Autoconf macros can define.
+   No software package will use all of them; autoheader copies the ones
+   your configure.in uses into your configuration header file templates.
+
+   The entries are in sort -df order: alphabetical, case insensitive,
+   ignoring punctuation (such as underscores).  Although this order
+   can split up related entries, it makes it easier to check whether
+   a given entry is in the file.
+
+   Leave the following blank line there!!  Autoheader needs it.  */
+
+
+/* Set this to be blah blah blah. */
+#undef DEBUG_LOG
+
+/* Set this blah blah blah. */
+#undef SCORESDIR
+
+/* Comment this out to use a shared best times file. */
+#undef USE_GROUP_BEST_FILE
+
+
+/* Leave that blank line there!!  Autoheader needs it.
+   If you're adding to this file, keep in mind:
+   The entries are in sort -df order: alphabetical, case insensitive,
+   ignoring punctuation (such as underscores).  */
