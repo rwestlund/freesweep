0a26431501fba6cb1f44014019bae6a5e80e9ee7
hartmann <hartmann@ae8a25bf-5f0b-68bf-7ac2-24876b81758e>
2002-12-20 08:44:53 +0000

    Fixed all sorts of color goo. There were two duplicate blue/black entries, and the rules for number->colors wasn't as good as it could have been.

--- trunk/drawing.c
+++ trunk/drawing.c
@@ -4,7 +4,7 @@
 *  License, version 2 or above; see the file COPYING for more         *
 *  information.                                                       *
 *                                                                     *
-*  $Id: drawing.c,v 1.22 2002-12-19 07:04:05 hartmann Exp $
+*  $Id: drawing.c,v 1.23 2002-12-20 08:44:53 hartmann Exp $
 *                                                                     *
 **********************************************************************/
 
@@ -19,19 +19,18 @@ void StartCurses()
 	if (has_colors())
 	{
 		start_color();
-		init_pair(1,COLOR_CYAN,COLOR_BLACK);
-		init_pair(2,COLOR_MAGENTA,COLOR_BLACK);
-		init_pair(3,COLOR_BLUE,COLOR_BLACK);
+		init_pair(1,COLOR_WHITE,COLOR_BLACK);
+		init_pair(2,COLOR_BLUE,COLOR_BLACK);
+		init_pair(3,COLOR_CYAN,COLOR_BLACK);
 		init_pair(4,COLOR_GREEN,COLOR_BLACK);
-		init_pair(5,COLOR_RED,COLOR_BLACK);
-		init_pair(6,COLOR_WHITE,COLOR_BLACK);
-		init_pair(7,COLOR_BLUE,COLOR_BLACK);
-		init_pair(8,COLOR_CYAN,COLOR_WHITE);
-		init_pair(9,COLOR_MAGENTA,COLOR_WHITE);
-		init_pair(10,COLOR_BLUE,COLOR_WHITE);
-		init_pair(11,COLOR_GREEN,COLOR_WHITE);
+		init_pair(5,COLOR_MAGENTA,COLOR_BLACK);
+		init_pair(6,COLOR_RED,COLOR_BLACK);
+		init_pair(7,COLOR_BLACK,COLOR_WHITE);
+		init_pair(8,COLOR_BLUE,COLOR_WHITE);
+		init_pair(9,COLOR_CYAN,COLOR_WHITE);
+		init_pair(10,COLOR_GREEN,COLOR_WHITE);
+		init_pair(11,COLOR_MAGENTA,COLOR_WHITE);
 		init_pair(12,COLOR_RED,COLOR_WHITE);
-		init_pair(13,COLOR_BLACK,COLOR_WHITE);
 	}
 	noecho();
 	keypad(stdscr, TRUE);
@@ -686,37 +685,15 @@ int DrawBoard(GameStats* Game)
 				case 1:
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,7,NULL);
-					}
-					waddch(Game->Board,(CellVal)+'0');
-					if (Game->Color != 0 && (has_colors() == TRUE))
-					{
-						wcolor_set(Game->Board,6,NULL);
-					}
-					break;
-				case 2:
-					if (Game->Color != 0 && (has_colors() == TRUE))
-					{
-						wcolor_set(Game->Board,1,NULL);
-					}
-					waddch(Game->Board,(CellVal)+'0');
-					if (Game->Color != 0 && (has_colors() == TRUE))
-					{
-						wcolor_set(Game->Board,6,NULL);
-					}
-					break;
-				case 3:
-					if (Game->Color != 0 && (has_colors() == TRUE))
-					{
 						wcolor_set(Game->Board,2,NULL);
 					}
 					waddch(Game->Board,(CellVal)+'0');
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,6,NULL);
+						wcolor_set(Game->Board,1,NULL);
 					}
 					break;
-				case 4:
+				case 2:
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
 						wcolor_set(Game->Board,3,NULL);
@@ -724,40 +701,62 @@ int DrawBoard(GameStats* Game)
 					waddch(Game->Board,(CellVal)+'0');
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,6,NULL);
+						wcolor_set(Game->Board,1,NULL);
+					}
+					break;
+				case 3:
+					if (Game->Color != 0 && (has_colors() == TRUE))
+					{
+						wcolor_set(Game->Board,4,NULL);
+					}
+					waddch(Game->Board,(CellVal)+'0');
+					if (Game->Color != 0 && (has_colors() == TRUE))
+					{
+						wcolor_set(Game->Board,1,NULL);
+					}
+					break;
+				case 4:
+					if (Game->Color != 0 && (has_colors() == TRUE))
+					{
+						wcolor_set(Game->Board,5,NULL);
+					}
+					waddch(Game->Board,(CellVal)+'0');
+					if (Game->Color != 0 && (has_colors() == TRUE))
+					{
+						wcolor_set(Game->Board,1,NULL);
 					}
 					break;
 				case 5:
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,1,NULL);
+						wcolor_set(Game->Board,6,NULL);
 					}
 					waddch(Game->Board,(CellVal)+'0');
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,6,NULL);
+						wcolor_set(Game->Board,1,NULL);
 					}
 					break;
 				case 6:
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,1,NULL);
+						wcolor_set(Game->Board,6,NULL);
 					}
 					waddch(Game->Board,(CellVal)+'0');
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,6,NULL);
+						wcolor_set(Game->Board,1,NULL);
 					}
 					break;
 				case 7:
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,1,NULL);
+						wcolor_set(Game->Board,6,NULL);
 					}
 					waddch(Game->Board,(CellVal)+'0');
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,6,NULL);
+						wcolor_set(Game->Board,1,NULL);
 					}
 					break;
 				case 8:
@@ -768,7 +767,7 @@ int DrawBoard(GameStats* Game)
 					waddch(Game->Board,(CellVal)+'0');
 					if (Game->Color != 0 && (has_colors() == TRUE))
 					{
-						wcolor_set(Game->Board,6,NULL);
+						wcolor_set(Game->Board,1,NULL);
 					}
 					break;
 				case MINE:
