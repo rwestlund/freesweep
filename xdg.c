edee7a4facbab6b11013ce9dc4cdfcc346f4a89c
Ron Wills <ron@digitalcombine.ca>
2024-02-22 05:50:26 -0600

    Improve memory allocations

--- xdg.c
+++ xdg.c
@@ -1,13 +1,33 @@
-/**********************************************************************
- *  This source code is copyright 2024 by Ron R Wills		      *
- *  It may be distributed under the terms of the GNU General Purpose  *
- *  License, version 2 or above; see the file COPYING for more	      *
- *  information.						      *
- *								      *
- **********************************************************************/
+/*                                                                    -*- c -*-
+ * Support for XDG base directory specifications.
+ * Copyright (C) 2024  Ron Wills <ron@digitalcombine.ca>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The fifo will no fail if there is a memory error. If there is a memory error
+ * a static message will get added to the end of the fifo and any new messages
+ * will get dropped. Even after an error occurs new messages will still attempt
+ * to be added to the end of the fifo incase system memory was freed.
+ */
 
 #include "sweep.h"
 #include <string.h>
+
+/*******************
+ * xdg_config_home *
+ *******************/
 
 char *xdg_config_home() {
   char *home = getenv("HOME");
@@ -23,14 +43,11 @@ char *xdg_config_home() {
     /* XDG_CONFIG_HOME hasn't been set so use the default location and make
      * sure it exists if possible.
      */
-    char *result = (char*)xmalloc(MAX_LINE);
+    int len = strlen(home) + strlen("/.config/freesweep") + 1;
+    char *result = (char*)xmalloc(len);
 
     // Make sure $HOME/.config exists
-#if defined(HAVE_SNPRINTF)
-    snprintf(result, MAX_LINE, "%s/.config", home);
-#else
-    sprintf(result, "%s/.config", home);
-#endif
+    snprintf(result, len, "%s/.config", home);
     if (!xexists(result)) {
       if (mkdir(result, 0750) == -1) {
         log_error("Unable to create the directory $HOME/.config");
@@ -53,13 +70,10 @@ char *xdg_config_home() {
      * this directory exists we only attempt to create the freesweep directory
      * within it.
      */
-    char *result = (char*)xmalloc(MAX_LINE);
+    int len = strlen(home_config) + strlen("/freesweep") + 1;
+    char *result = (char*)xmalloc(len);
 
-#if defined(HAVE_SNPRINTF)
-    snprintf(result, MAX_LINE, "%s/freesweep", home_config);
-#else
-    sprintf(result, "%s/freesweep", home_config);
-#endif
+    snprintf(result, len, "%s/freesweep", home_config);
 
     if (!xexists(result)) {
       if (mkdir(result, 0750) == -1) {
@@ -72,12 +86,16 @@ char *xdg_config_home() {
   }
 }
 
+/*****************
+ * xdg_data_home *
+ *****************/
+
 char *xdg_data_home() {
   char *home = getenv("HOME");
   char *home_data = getenv("XDG_DATA_HOME");
 
   if (home == NULL) {
-    perror("$HOME not set");
+    log_error("$HOME not set");
     return NULL;
   }
 
@@ -85,17 +103,15 @@ char *xdg_data_home() {
     /* XDG_DATA_HOME hasn't been set so use the default location and make
      * sure it exists if possible.
      */
-    char *result = (char*)xmalloc(MAX_LINE);
+    int len = strlen(home) + strlen("/.local/share/freesweep") + 1;
+    char *result = (char*)xmalloc(len);
+
+    snprintf(result, len, "%s/.local", home);
 
     // Make sure $HOME/.local exists
-#if defined(HAVE_SNPRINTF)
-    snprintf(result, MAX_LINE, "%s/.local", home);
-#else
-    sprintf(result, "%s/.local", home);
-#endif
     if (!xexists(result)) {
       if (mkdir(result, 0750) == -1) {
-        perror("Unable to create the directory $HOME/.local");
+        log_error("Unable to create the directory $HOME/.local");
         return NULL;
       }
     }
@@ -104,7 +120,7 @@ char *xdg_data_home() {
     strncat(result, "/share", MAX_LINE);
     if (!xexists(result)) {
       if (mkdir(result, 0750) == -1) {
-        perror("Unable to create the directory $HOME/.local/share");
+        log_error("Unable to create the directory $HOME/.local/share");
         return NULL;
       }
     }
@@ -113,24 +129,26 @@ char *xdg_data_home() {
     strncat(result, "/freesweep", MAX_LINE);
     if (!xexists(result)) {
       if (mkdir(result, 0750) == -1) {
-        perror("Unable to create the directory $HOME/.local/share/freesweep");
+        log_error("Unable to create the directory "
+                  "$HOME/.local/share/freesweep");
         return NULL;
       }
     }
 
     return result;
   } else {
-    char *result = (char*)xmalloc(MAX_LINE);
+    /* XDG_DATA_HOME has been set. I beleive it is up to the user to ensure
+     * this directory exists we only attempt to create the freesweep directory
+     * within it.
+     */
+    int len = strlen(home_data) + strlen("/freesweep") + 1;
+    char *result = (char*)xmalloc(len);
 
-#if defined(HAVE_SNPRINTF)
-    snprintf(result, MAX_LINE, "%s/freesweep", home_data);
-#else
-    sprintf(result, "%s/freesweep", home_data);
-#endif
+    snprintf(result, len, "%s/freesweep", home_data);
 
     if (!xexists(result)) {
       if (mkdir(result, 0750) == -1) {
-        perror("Unable to create the directory $XDG_DATA_HOME/freesweep");
+        log_error("Unable to create the directory $XDG_DATA_HOME/freesweep");
         return NULL;
       }
     }
